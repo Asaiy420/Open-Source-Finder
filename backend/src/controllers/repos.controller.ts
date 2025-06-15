f1f1845930bea0b4b5ad5ef091e6c01042eafc09
Asaiy420 <kcaryan05@gmail.com>
2025-06-15 10:04:34 +0545

    add: repos controller

--- backend/src/controllers/repos.controller.ts
+++ backend/src/controllers/repos.controller.ts
@@ -1,5 +1,46 @@
 import { Request, Response } from "express";
+import axios from "axios";
 
-export const getRepos = (req: Request, res: Response) => {
+export const getRepos = async (req: Request, res: Response):Promise<void> => {
+    const {topic, stars, language, per_page=10, page = 1} = req.query;
+
+    try {
+        if (!language || !topic || !stars){
+            res.status(400).json({
+                error: "At least one filter (topic, stars, language) is required"
+            })
+            return;
+        }
     
+        let q = "";
+        if (topic) q += `topic:${topic}`; 
+        if (language) q += `language:${language}`; 
+        if (stars) q += `stars:${stars}`; 
+    
+        q = q.slice(0, -1); // removes trailing '+'
+
+
+        const response = await axios.get("https://api.github.com/search/repositories", {
+            params: {
+                q,
+                sort: "stars", // sorts by stars popularity
+                per_page,
+                page
+            }
+        }); 
+
+        res.status(200).json({
+            total: response.data.total_count,
+            repos: response.data.items,
+            page,
+            per_page
+        })
+    
+    } catch (error) {
+        
+    }
+
+
+
+
 }
